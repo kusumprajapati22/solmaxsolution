05bac19505a963c167858b464a402597aa35fa95
Kusum <kusum70111@gmail.com>
2025-01-23 15:41:35 +0530

    solmaxsolution

---
+++ send_message.php
@@ -0,0 +1,34 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars(trim($_POST['name']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $number = htmlspecialchars(trim($_POST['number']));
+    $project = htmlspecialchars(trim($_POST['project']));
+    $subject = htmlspecialchars(trim($_POST['subject']));
+    $message = htmlspecialchars(trim($_POST['message']));
+    
+    $to = "solmaxsolution500@gmail.com";
+    $subject = "Your Mall Contact Us Form ";
+    
+    $email_body = "You have received a new message:\n\n";
+    $email_body .= "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Number: $number\n";
+    $email_body .= "Project: $project\n";
+    $email_body .= "Subject: $subject\n";
+    $email_body .= "Message: $message\n";
+
+
+    $headers = "From: $email\n";
+    $headers .= "Reply-To: $email\n";
+    
+    if (mail($to, $subject, $email_body, $headers)) {
+        header("Location: thank_you1.php");
+        exit();
+    } else {
+        echo "There was an error sending your message. Please try again.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
